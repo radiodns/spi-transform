21e9f6701957487ca69112559d27a8b126e434ea
Andy Buckingham <andy@andybee.com>
2017-08-12 12:52:05 +0100

    Reworked diff script (closes #8)
    Even with xmllint formatting and canonicalisation, diff results were still throwing up incorrect "errors". Have simplified the script to use an alternative Python library which gave more appropriate results in testing. Also improved path support so the test script can be called from anywhere.

--- test/diff.sh
+++ test/diff.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
-# This script takes example documents of one format, applies the transform and
-# then diffs the output with an example of the then transformed format.
+# This script takes example documents of each format, applies the appropriate
+# transform and then diffs the output with an example of the opposing format.
+# Thus, both documents should be canonically identical.
 
-# Both examples should be examples of all common elements and attributes and,
-# most importantly, be valid to ensure the transform operation is valid.
-
-# xmllint is used with the --exc-c14n profile to ensure minor but unimportant
-# differences, such as element or attribute order, or different but technically
-# identical approaches to namespace declaration, are ignored
-
-# It requires both xsltproc and xmllint to be available on PATH and should be
-# run from the parent project directory.
+# It requires xsltproc[1] and xmldiffs[2] to be available on PATH.
 
 # As with a standard diff, no output implies the documents are identical.
 
-echo 'Diffing Example XSI-to-SI transform output against example SI'
-diff <(xsltproc xsi_to_si.xsl test/XSI.example.xml | xmllint --format --exc-c14n -) <(xmllint --format --exc-c14n test/SI.example.xml)
+# [1] http://xmlsoft.org/XSLT/xsltproc2.html
+# [2] https://github.com/joh/xmldiffs
 
-echo 'Diffing Example SI-to-XSI transform output against example XSI'
-diff <(xsltproc si_to_xsi.xsl test/SI.example.xml | xmllint --format --exc-c14n -) <(xmllint --format --exc-c14n test/XSI.example.xml)
+TEST_PATH=`dirname $0`
+xmldiffs <(xsltproc $TEST_PATH/../xsi_to_si.xsl $TEST_PATH/XSI.example.xml) $TEST_PATH/SI.example.xml
+xmldiffs <(xsltproc $TEST_PATH/../si_to_xsi.xsl $TEST_PATH/SI.example.xml) $TEST_PATH/XSI.example.xml
